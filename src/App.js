048dc1104c6500b8f06f6468e8a3e5b45f2ce136
NaifatSelina <naifatselina@icloud.com>
2023-07-10 21:02:40 +0000

    Add navbar to app and add react ootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import styles from "./App.module.css";
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      < NavBar />
     </div>
   );
 }
