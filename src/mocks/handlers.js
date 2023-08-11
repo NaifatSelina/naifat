6fe96460e450cb7bde427bcf08ef0b09e6fe57fe
NaifatSelina <naifatselina@icloud.com>
2023-08-11 22:24:22 +0000

    add some styling

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,19 +1,19 @@
 import { rest } from "msw";
 
-const baseURL = "https://drf-api-rec.herokuapp.com/";
+const baseURL = "https://drf-api-app-65d9b6a619df.herokuapp.com/";
 
 export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
     return res(
       ctx.json({
         pk: 2,
-        username: "brian",
+        username: "user",
         email: "",
         first_name: "",
         last_name: "",
         profile_id: 2,
         profile_image:
-          "https://res.cloudinary.com/dgjrrvdbl/image/upload/v1/media/../default_profile_qdjgyp",
+          "https://res.cloudinary.com/dxs8nfjnp/image/upload/v1688116599/samples/default_profile_nqohxy.jpg",
       })
     );
   }),
